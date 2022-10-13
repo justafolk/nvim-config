3d3d9e6a60d728710824e6ee9033c2c4e2379424
Avdhut Kamble <65607123+justafolk@users.noreply.github.com>
2022-10-13 09:24:02 +0530

    DONT USE THIS FILE

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+# DONT USE THIS FILE
+
 rm -rf ~/.config/nvim/
 cp -r ./nvim ~/.config/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
