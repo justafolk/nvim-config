7e3042eba2e953b035c0bb6d5a37a27bad8f71e6
Avdhut Kamble <avdhut.kamble776@gmail.com>
2022-10-22 23:04:35 +0530

    add init files

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -53,10 +53,11 @@ protocol.CompletionItemKind = {
   '', -- TypeParameter
 }
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
+-- local capabilities = require('cmp_nvim_lsp').update_capabilities(
+--  vim.lsp.protocol.make_client_capabilities()
+--)
 
 nvim_lsp.flow.setup {
   on_attach = on_attach,
